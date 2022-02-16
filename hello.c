9c1d7a327e63bb5dc8d6939ee10b686b2cb7b60c
jiehwan <jiehwan.park@gmail.com>
2022-02-16 11:12:01 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,10 +1,12 @@
-// test2
+// test3
 #include <stdio.h>
 
 int main(int A, int *B, int *args) {
-  while{1) {
+  while{1) 
+  {
   printf("hello june!\n");
   }
-  if(1) { printf("hello july!\n"); }
+  if(1) 
+  { printf("hello july!\n"); }
   return 0;
 }
